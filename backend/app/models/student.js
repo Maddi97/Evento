c8667d570191541161636f3ffbd39e559d9e5bc7
Maddi <sgdmaddi.97@gmail.com>
2020-08-15 18:59:56 +0200

    vzk

--- backend/app/models/student.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require("mongoose");
-
-// define our students model
-// module.exports allows us to pass this to other files when it is called
-module.exports = mongoose.model("Student", {
-  name: { type: String, default: "" },
-});
--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,20 @@
 // modules =================================================
 const express = require("express");
+const event_routes = require("./app/routes/event.routes");
 const app = express();
 var mongoose = require("mongoose");
 // set our port
 const port = 3000;
 // configuration ===========================================
 
+app.use("/api/", router);
+
 // config files
 var db = require("./config/db");
 console.log("connecting--", db);
 mongoose.connect(db.url); //Mongoose connection created
 
-// frontend routes =========================================================
-app.get("/", (req, res) => res.send("Welcome to Tutorialspoint!"));
-
-//defining route
-app.get("/tproute", function (req, res) {
-  res.send(
-    "This is routing for the application developed using Node and Express..."
-  );
-});
-
-// sample api route
-// grab the student model we just created
-var Student = require("./app/models/student");
-app.get("/api/students", function (req, res) {
-  // use mongoose to get all students in the database
-  Student.find(function (err, students) {
-    // if there is an error retrieving, send the error.
-    // nothing after res.send(err) will execute
-    if (err) res.send(err);
-    res.json(students); // return all students in JSON format
-  });
-});
 // startup our app at http://localhost:3000
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () =>
+  console.log(`3vents51 backend listening on port ${port}!`)
+);
