eabea4fb2134fa008e6dc55dfcfa97decc730371
Maddi97 <sgdmaddi.97@gmail.com>
2020-08-29 13:00:56 +0200

    eintragen funktioniert

--- insertApp/insert-app/src/app/pages/organizer-view/organizer-view.component.ts
+++ insertApp/insert-app/src/app/pages/organizer-view/organizer-view.component.ts
@@ -21,6 +21,8 @@ export class OrganizerViewComponent implements OnInit {
     street: new FormControl('', []),
     streetNumber: new FormControl('', []),
     country: new FormControl('Deutschland', []),
+    email: new FormControl('', []),
+    telephone: new FormControl('', []),
     category: new FormControl('', []),
     description: new FormControl('', [])
   })
@@ -64,6 +66,8 @@ export class OrganizerViewComponent implements OnInit {
 
     org.adress = adress
 
+    org.email = this.organizerForm.get('email').value;
+    org.telephone = this.organizerForm.get('telephone').value;
     org.description = this.organizerForm.get('description').value;
     org.category = this.organizerForm.get('category').value;
 
@@ -83,6 +87,8 @@ export class OrganizerViewComponent implements OnInit {
       street: org.adress.street,
       streetNumber: org.adress.streetNumber,
       country: org.adress.country,
+      email: org.email,
+      telephone: org.telephone,
       description: org.description,
       category: org.category
   });
@@ -106,6 +112,8 @@ export class OrganizerViewComponent implements OnInit {
 
       org.adress = adress
 
+      org.email = this.organizerForm.get('email').value;
+      org.telephone = this.organizerForm.get('telephone').value;
       org.description = this.organizerForm.get('description').value;
       org.category = this.organizerForm.get('category').value;
 
@@ -120,11 +128,13 @@ export class OrganizerViewComponent implements OnInit {
   nullFormField(){
     this.organizerForm.setValue({
       name : '',
-      city : '',
+      city : 'Dresden',
       plz  : '',
       street: '',
       streetNumber: '',
       country: '',
+      email: '',
+      telephone: '',
       description: '',
       category: ''
     });
