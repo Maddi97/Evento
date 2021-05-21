428e544f256199611ccecd768b8699deba4375b7
Maximilian Schaller <ms80xuxu@studserv.uni-leipzig.de>
2021-05-21 16:30:45 +0200

    backend filname generation image upload

--- backend/routes/file.routes.js
+++ backend/routes/file.routes.js
@@ -1,24 +1,33 @@
+
+var crypto = require("crypto");
 var express = require("express");
 var router = express.Router();
 const multer = require('multer');
 
-var DIR = './uploads/';
 
-var upload = multer({dest: DIR}).single('file');
-
-
-
-
-router.post('/uploadFile', function (req, res, next) {
-     upload(req, res, function (err) {   
-        if (err) {
-          // An error occurred when uploading
-          console.log(err);
-          return res.status(422).send("an Error occured")
-        }  
-       // No error occured.
-        return res.send(req.file); 
-  });  
+const storage = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null, 'uploads');
+  },
+  filename: function(req, file, cb) {
+    cb(
+      null,
+      `${crypto.randomBytes(10).toString("hex")}_${Date.now()}_${
+        file.originalname
+      }`
+    );
+  }
 })
+
+const upload = multer({ storage });
+
+router.post('/uploadFile', upload.single('file'), function (req, res, next) {
+      const file = req.file
+       if(file){
+         res.json(file)
+       } else {
+         throw new Error("File upload unsuccessfull")
+       }
+  });  
 
 module.exports = router;
