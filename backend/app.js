46b4cb3a4f8356e6ac00573aaeb82755fe6efab2
Maximilian Schaller <ms80xuxu@studserv.uni-leipzig.de>
2022-04-17 15:06:45 +0200

    android app working

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,32 @@ var cors = require('cors')
 // configuration ===========================================
 const mongoose = require("./config/mongoose")
 
+const allowedOrigins = [
+    'capacitor://localhost',
+    'ionic://localhost',
+    'http://localhost',
+    'http://localhost:8080',
+    'http://localhost:8100',
+];
+
+// Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Origin not allowed by CORS'));
+        }
+    },
+};
+
+// Enable preflight requests for all routes
+app.options('*', cors(corsOptions));
+
+app.get('/', cors(corsOptions), (req, res, next) => {
+    res.json({message: 'This route is CORS-enabled for an allowed origin.'});
+});
+
 app.use(express.json())
 app.use(cors())
 
@@ -25,5 +51,5 @@ var fileRouter = require('./routes/file.routes')
 app.use(fileRouter)
 
 app.listen(port, () =>
-  console.log(`3vents51 backend listening on port ${port}!`)
+    console.log(`3vents51 backend listening on port ${port}!`)
 );
---
+++ threeventsfiveone/android/README.md
@@ -0,0 +1,6 @@
+## Backend calls in development
+
+change apiBaseURL from http://localhost:3000 to http://10.0.2.2:3000
+
+because app is running in an emulator and localhost calls the emulator itself but backend is runing on local pc so we
+need to redirect this
