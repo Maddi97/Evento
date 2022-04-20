fbb431d518a4fb25e9d9c307a11ae003a1e3c1b8
Maximilian Schaller <ms80xuxu@studserv.uni-leipzig.de>
2022-04-20 14:54:44 +0200

    stable solution for ios

--- threeventsfiveone/src/app/common-utilities/social-media-share/social-media-share.component.ts
+++ threeventsfiveone/src/app/common-utilities/social-media-share/social-media-share.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import {DomSanitizer} from '@angular/platform-browser';
-
+import {Share} from "@capacitor/share";
+import {DeviceDetectorService} from "ngx-device-detector";
 
 @Component({
   selector: 'vents-social-media-share',
@@ -13,19 +14,31 @@ export class SocialMediaShareComponent implements OnInit {
   url: string = window.location.href;
   shareText = 'Schau dir dieses Event an, dass ich auf 3vents51 gefunden habe!';
   whatsappLink = encodeURI(`https://wa.me?text=${encodeURIComponent(this.shareText + '/n' + this.url)}`);
+  isDeviceMobile: boolean;
 
   constructor(private _snackbar: MatSnackBar,
-              private sanitizer: DomSanitizer
+              private sanitizer: DomSanitizer,
+              private deviceService: DeviceDetectorService
   ) {
 
   }
 
   ngOnInit(): void {
-
+    this.isDeviceMobile = this.deviceService.isMobile()
   }
 
   sanitize(url: string) {
     return this.sanitizer.bypassSecurityTrustUrl(url);
+  }
+
+  async shareOnPhone() {
+    await Share.share({
+      title: 'Share Event',
+      text: this.shareText,
+      url: this.url,
+      dialogTitle: 'Teile das Event mit Freunden',
+    }).then(() => console.log('Sharing successful'))
+
   }
 
   openSnackBar(message) {
