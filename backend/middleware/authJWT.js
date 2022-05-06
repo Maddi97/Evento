33a6367f5aab18778336b7ae5cbb924f31ac5729
Maximilian Schaller <ms80xuxu@studserv.uni-leipzig.de>
2022-05-06 16:37:32 +0200

    secured routes with jwt

--- backend/middleware/authJWT.js
+++ backend/middleware/authJWT.js
@@ -1,19 +1,20 @@
 const jwt = require("jsonwebtoken");
 const config = require("../config/auth.config.js");
-const db = require("../model/user.model");
 
-verifyToken = (req, res, next) => {
-    let token = req.headers["x-access-token"];
+const verifyToken = (req, res, next) => {
+    const token =
+        req.body.token || req.query.token || req.headers["x-access-token"];
+
     if (!token) {
-        return res.status(403).send({message: "No token provided!"});
+        return res.status(403).send("A token is required for authentication");
     }
-    jwt.verify(token, config.secret, (err, decoded) => {
-        if (err) {
-            return res.status(401).send({message: "Unauthorized!"});
-        }
-        req.userId = decoded.id;
-        next();
-    });
+    try {
+        const decoded = jwt.verify(token, config.secret);
+        req.user = decoded;
+    } catch (err) {
+        return res.status(401).send("Invalid Token");
+    }
+    return next();
 };
 
 module.exports = verifyToken;
--- backend/routes/event.routes.js
+++ backend/routes/event.routes.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const {verifyToken} = require("../middleware");
+const auth = require("../middleware/authJWT");
 
 const Event = require("../model/event.model")
 
@@ -140,7 +140,7 @@ router.post('/getEventsOnCategory', (req, res) => {
 })
 
 
-router.post('/organizer/:organizerId/events', verifyToken, (req, res) => {
+router.post('/organizer/:organizerId/events', auth, (req, res) => {
     (new Event(req.body.event))
         .save()
         .then((events) => res.send(events))
@@ -153,7 +153,7 @@ router.get('/organizer/:organizerId/events/:eventId', (req, res) => {
         .catch((error) => console.log(error))
 });
 
-router.patch('/organizer/:organizerId/events/:eventId', verifyToken, (req, res) => {
+router.patch('/organizer/:organizerId/events/:eventId', auth, (req, res) => {
     Event.findOneAndUpdate(
         {_organizerId: req.params.organizerId, _id: req.params.eventId},
         {$set: req.body.event}, {new: true}
@@ -162,7 +162,7 @@ router.patch('/organizer/:organizerId/events/:eventId', verifyToken, (req, res) 
         .catch((error) => console.log(error));
 });
 
-router.delete('/organizer/:organizerId/events/:eventId', verifyToken, (req, res) => {
+router.delete('/organizer/:organizerId/events/:eventId', auth, (req, res) => {
     Event.findOneAndDelete({_organizerId: req.params.organizerId, _id: req.params.eventId})
         .then((event) => res.send(event))
         .catch((error) => console.log(error))
