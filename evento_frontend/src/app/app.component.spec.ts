d422c3270b63e2fb4aeacd977fb706ec24058d2b
Maddi97 <sgdmaddi.97@gmail.com>
2023-02-03 10:06:10 +0100

    update angular to cli 14

--- evento_frontend/src/app/app.component.spec.ts
+++ evento_frontend/src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {TestBed, async} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'threeventsfiveone'`, () => {
+  it(`should have as title 'evento_frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('threeventsfiveone');
+    expect(app.title).toEqual('evento_frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('threeventsfiveone app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('evento_frontend app is running!');
   });
 });
