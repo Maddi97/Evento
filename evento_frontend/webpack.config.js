9c6f8fbbfb328e8826c346d3177d644ad3b0315c
Maddi97 <sgdmaddi.97@gmail.com>
2023-02-03 09:58:04 +0100

    try to update ng and add webpack

---
+++ evento_frontend/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
