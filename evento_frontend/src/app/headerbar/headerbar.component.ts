32a29a08365376986bc4416ea39c0e18da6151ac
mschaller <Maximilian.Schaller@jambit.com>
2023-08-17 12:59:19 +0200

    header logo full event page

--- evento_frontend/src/app/headerbar/headerbar.component.ts
+++ evento_frontend/src/app/headerbar/headerbar.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {Location} from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 import * as moment from 'moment';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-headerbar',
@@ -9,21 +11,34 @@ import * as moment from 'moment';
 })
 export class HeaderbarComponent implements OnInit {
   searchText = '';
+  fullEventPage = false;
   filteredDate: moment.Moment = moment(new Date()).utcOffset(0, false).set({
     hour: 0,
     minute: 0,
     second: 0,
     millisecond: 0
   })
-  ;
+    ;
 
   constructor(
     private location: Location,
+    private router: Router, private activatedRoute: ActivatedRoute
   ) {
   }
 
   ngOnInit(): void {
-    
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      console.log(event.url)
+      console.log(event.url.includes('full-event'))
+      if (event.url.includes('full-event')) {
+        this.fullEventPage = true;
+      }
+      else {
+        this.fullEventPage = false;
+      }
+    });
   }
 
   navBack() {
