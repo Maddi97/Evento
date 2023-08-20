aa79003c3958be8d70cbb2321caf7f015a31be55
Maddi97 <sgdmaddi.97@gmail.com>
2023-08-20 14:27:59 +0200

    cache_images

--- evento_frontend/src/app/file.service.ts
+++ evento_frontend/src/app/file.service.ts
@@ -1,30 +1,33 @@
 import { Injectable } from '@angular/core';
 import {WebService} from './web.service';
-import {catchError, map, share} from 'rxjs/operators';
-import { Observable, throwError as observableThrowError, BehaviorSubject } from 'rxjs';
+import {catchError, map, share, tap} from 'rxjs/operators';
+import { Observable, throwError as observableThrowError, BehaviorSubject, of, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileService {
-  private _files: BehaviorSubject<any[]> = new BehaviorSubject(new Array<any>());
+  private cachedFiles: Map<string, Blob> = new Map();
 
   constructor(    private webService: WebService
   ) { }
 
   downloadFile(path: string): Observable<Blob> {
-    const obs = this.webService.get_file('downloadFile', {path}).pipe(
-      map((r) => r as unknown as any),
+    if (this.cachedFiles.has(path)) {
+      return of(this.cachedFiles.get(path)!);
+    }
+
+    const obs = this.webService.get_file('downloadFile', { path }).pipe(
+      map((response) => response as unknown as Blob),
       catchError((error: any) => {
-        console.error('an error occurred', error);
-        return observableThrowError(error.error.message || error);
+        console.error('An error occurred', error);
+        return throwError(error.error.message || error);
       }),
-      share());
-    obs.toPromise().then(
-      (response: any) => {
-      }
+      tap((blob) => {
+        this.cachedFiles.set(path, blob);
+      }),
+      share()
     );
     return obs;
   }
-
 }
