2d0adc31629ea23860c24ef394e34ddff06b250e
Maddi97 <sgdmaddi.97@gmail.com>
2023-08-20 20:59:31 +0200

    cache organizer and events important routes

--- evento_frontend/src/app/events/event.service.ts
+++ evento_frontend/src/app/events/event.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
-import {BehaviorSubject, Observable, throwError as observableThrowError} from 'rxjs';
+import {BehaviorSubject, Observable, throwError as observableThrowError, of, throwError} from 'rxjs';
 import {Event} from '../models/event';
 import {WebService} from '../web.service';
-import {filter, map, catchError, share, switchMap} from 'rxjs/operators';
+import {filter, map, catchError, share, switchMap, tap} from 'rxjs/operators';
 import {HttpRequest} from '@angular/common/http';
 import {Organizer} from '../models/organizer';
 import * as moment from 'moment';
@@ -14,6 +14,7 @@ import * as moment from 'moment';
 export class EventService {
 
   private _events: BehaviorSubject<Event[]> = new BehaviorSubject(new Array<Event>());
+  private cachedEvents: Map<string, Event[]> = new Map();
 
   constructor(
     private webService: WebService,
@@ -85,6 +86,12 @@ export class EventService {
   }
 
   getAllUpcomingEvents(): Observable<Event[]> {
+    const date = new Date()
+    const cacheKey = JSON.stringify(date);
+    if (this.cachedEvents.has(cacheKey)) {
+      this._events.next(this.cachedEvents.get(cacheKey))
+      return of(this.cachedEvents.get(cacheKey)!);
+    }
     const obs = this.webService.get('upcomingEvents').pipe(
       map((res: HttpRequest<any>) => res as unknown as Event[]),
       catchError((error: any) => {
@@ -112,19 +119,32 @@ export class EventService {
     return obs;
   }
 
-  getEventsOnDateCategoryAndSubcategory(fil: any): Observable<Event[]> {
-    const obs = this.webService.post('eventOnDateCatAndSubcat', {fil}).pipe(
-      map((res: HttpRequest<any>) =>
-        res as unknown as Event[]),
+getEventsOnDateCategoryAndSubcategory(fil: any): Observable<Event[]> {
+    const cacheKey = JSON.stringify(fil);
+    if (this.cachedEvents.has(cacheKey)) {
+      this._events.next(this.cachedEvents.get(cacheKey))
+      return of(this.cachedEvents.get(cacheKey)!);
+    }
+
+    const obs = this.webService.post('eventOnDateCatAndSubcat', { fil }).pipe(
+      map((res: any) => res as Event[]),
       catchError((error: any) => {
-        console.error('an error occured', error);
-        return observableThrowError(error.error.message || error);
+        console.error('An error occurred', error);
+        return throwError(error.error.message || error);
       }),
-      share());
-    obs.toPromise().then((response) => {
-      this._events.next(response);
-    });
+      tap((events) => {
+        this.cachedEvents.set(cacheKey, events);
+        this._events.next(events);
+      }),
+      share()
+    );
+      obs.toPromise().then((response) => {
+          console.log(response)
+      });
     return obs;
   }
 
+  get cachedEventList(): Observable<Event[]> {
+    return this._events.asObservable();
+  }
 }
