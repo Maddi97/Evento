73b130cffd83e6a8041eb4e303a05a000fcf1dd3
Maddi97 <sgdmaddi.97@gmail.com>
2023-08-25 11:31:24 +0200

    env file

--- backend/config/auth.config.js
+++ backend/config/auth.config.js
@@ -1,7 +1,4 @@
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
-
 module.exports = {
-    secret: process.env.SECRET,
-    signupkey: process.env.SIGNUPKEY
+  secret: process.env.SECRET,
+  signupkey: process.env.SIGNUPKEY,
 };
