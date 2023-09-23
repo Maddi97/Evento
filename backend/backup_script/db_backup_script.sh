f63316ff52a33dabedd74cc92acdf0f4fe517bdc
mschaller <Maximilian.Schaller@jambit.com>
2023-09-23 16:35:09 +0200

    backup scripts

--- backend/backup_script/db_backup_script.sh
+++ backend/backup_script/db_backup_script.sh
@@ -14,8 +14,8 @@ TODAY=`date +"%d%b%Y"`
 
 ######################################################################
 ######################################################################
-
-DB_BACKUP_PATH='/home/evento/backup/mongodb_backup'
+BACKUP_PATH_MONGO='/backup/daily_backup'
+BACKUP_PATH_SEVRVER='/home/evento/backup/mongodb_backup'
 MONGO_HOST='0.0.0.0'
 MONGO_PORT='27017'
 
@@ -41,7 +41,7 @@ BACKUP_RETAIN_DAYS=150
 ######################################################################
 ######################################################################
 
-mkdir -p ${DB_BACKUP_PATH}/${TODAY}
+mkdir -p ${BACKUP_PATH_SEVRVER}/${TODAY}
 
 AUTH_PARAM=""
 
@@ -49,17 +49,11 @@ if [ ${AUTH_ENABLED} -eq 1 ]; then
  AUTH_PARAM=" --username ${MONGO_USER} --password ${MONGO_PASSWD} "
 fi
 
-if [ ${DATABASE_NAMES} = "ALL" ]; then
- echo "You have choose to backup all databases"
-docker exec -i mongodb mongodump --host ${MONGO_HOST} --port ${MONGO_PORT} ${AUTH_PARAM} --out ${DB_BACKUP_PATH}/${TODAY}/
-docker cp mongodb:/${DB_BACKUP_PATH}/${TODAY}/ ${DB_BACKUP_PATH}/${TODAY}/
-
-else
  echo "Running backup for selected databases"
  for DB_NAME in ${DATABASE_NAMES}
  do
- docker exec -i mongodb mongodump --host ${MONGO_HOST} --port ${MONGO_PORT} --db ${DB_NAME} ${AUTH_PARAM} --out ${DB_BACKUP_PATH}/${TODAY}/
- docker cp mongodb:/${DB_BACKUP_PATH}/${TODAY}/ ${DB_BACKUP_PATH}/${TODAY}/
+ docker exec -i mongodb mongodump --host ${MONGO_HOST} --port ${MONGO_PORT} --db ${DB_NAME} ${AUTH_PARAM} --out ${BACKUP_PATH_MONGO} --gzip
+ docker cp mongodb:/${BACKUP_PATH_MONGO}/ ${BACKUP_PATH_SEVRVER}/${TODAY}/
  done
 fi
 
@@ -69,8 +63,8 @@ fi
 
 DBDELDATE=`date +"%d%b%Y" --date="${BACKUP_RETAIN_DAYS} days ago"`
 
-if [ ! -z ${DB_BACKUP_PATH} ]; then
-      cd ${DB_BACKUP_PATH}
+if [ ! -z ${BACKUP_PATH_SEVRVER} ]; then
+      cd ${BACKUP_PATH_SEVRVER}
       if [ ! -z ${DBDELDATE} ] && [ -d ${DBDELDATE} ]; then
             rm -rf ${DBDELDATE}
       fi
--- backend/backup_script/restore_db.sh
+++ backend/backup_script/restore_db.sh
@@ -1,11 +1,11 @@
-DOCKER_CONTAINER = 'backend'
-IMAGE_PATH = 'app/images'
+DOCKER_CONTAINER='backend'
+IMAGE_PATH='app/images'
 
 docker cp /home/evento/backup/mongodb_backup/IMAGE_BACKUP/ ${DOCKER_CONTAINER}:/${IMAGE_PATH} 
 
 docker cp /home/evento/backup/mongodb_backup/${DATE_TO_RESTORE}/ mongodb:/backup
 
-docker exec -it mongodb bash -c "mongorestore --uri mongodb://mongodb:27017/db_evento backup/22Aug2023/db_evento"
+docker exec -it mongodb bash -c "mongorestore --gzip --uri mongodb://mongodb:27017/db_evento backup/22Aug2023/db_evento"
 #mongorestore -db db_evento backup/07Aug2023/db_evento
 
 #lokale
