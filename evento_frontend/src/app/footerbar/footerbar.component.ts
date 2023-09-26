67e0233b4f2b1480a669629efcdeb0d8e1e038c5
mschaller <Maximilian.Schaller@jambit.com>
2023-09-26 17:47:51 +0200

    css button clicks active and footer click

--- evento_frontend/src/app/footerbar/footerbar.component.ts
+++ evento_frontend/src/app/footerbar/footerbar.component.ts
@@ -1,6 +1,7 @@
-import { Component, HostListener, OnInit } from "@angular/core";
+import { Component, HostListener, Inject, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { SessionStorageService } from "../common-utilities/session-storage/session-storage.service";
+import { DOCUMENT } from "@angular/common";
 
 @Component({
   selector: "app-footerbar",
@@ -10,14 +11,16 @@ import { SessionStorageService } from "../common-utilities/session-storage/sessi
 export class FooterbarComponent implements OnInit {
   windowWidth;
   queryParams: any;
-
+  footerPickedField: 'settings' | 'categories' | 'events';
   constructor(
     private route: ActivatedRoute,
-    private sessionStorageService: SessionStorageService
-  ) {}
+    private sessionStorageService: SessionStorageService,
+    @Inject(DOCUMENT) private document: Document
+  ) { }
 
   ngOnInit(): void {
     this.windowWidth = window.innerWidth;
+    this.footerPickedField = this.getCurrentFooterPickedField()
     this.route.queryParams.subscribe((params) => {
       this.queryParams = params;
     });
@@ -30,5 +33,18 @@ export class FooterbarComponent implements OnInit {
 
   changeMapView() {
     this.sessionStorageService.setMapViewData(false);
+  }
+  getCurrentFooterPickedField() {
+    const url = this.document.location.pathname;
+    if (url.includes('settings')) {
+      return 'settings'
+    }
+    else if (url.includes('categories')) {
+      return 'categories'
+    }
+    else return 'events'
+  }
+  setFooterPickedField(footerPickedField: 'settings' | 'categories' | 'events') {
+    this.footerPickedField = footerPickedField;
   }
 }
