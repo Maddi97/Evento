7064e00137c96c08c0909a5e616b4f77efa3fa54
mschaller <Maximilian.Schaller@jambit.com>
2023-10-03 08:56:41 +0200

    restore adjustment

--- backend/backup_script/restore_db.sh
+++ backend/backup_script/restore_db.sh
@@ -1,8 +1,9 @@
 DOCKER_CONTAINER='backend'
 IMAGE_PATH='app/images'
-DATE='23Sep2023'
+DATE='26Sep2023'
 
 ssh "root@h2970439.stratoserver.net" "docker cp /home/evento/backup/mongodb_backup/IMAGE_BACKUP/ ${DOCKER_CONTAINER}:/${IMAGE_PATH}"
+ssh "root@h2970439.stratoserver.net" "docker exec mongodb bash -c 'mkdir /backup'"
 ssh "root@h2970439.stratoserver.net" ""docker cp /home/evento/backup/mongodb_backup/${DATE}/ mongodb:/backup/${DATE}/""
 
 ssh "root@h2970439.stratoserver.net" "docker exec mongodb bash -c 'mongorestore --gzip --uri mongodb://mongodb:27017/db_evento backup/${DATE}/daily_backup/db_evento'"
