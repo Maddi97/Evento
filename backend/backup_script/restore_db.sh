2cb6bfbeb37f85d3b2b11b24025fea6b7864b921
mschaller <Maximilian.Schaller@jambit.com>
2023-10-12 16:27:21 +0200

    local backup restore script and new subdomain

--- backend/backup_script/restore_db.sh
+++ backend/backup_script/restore_db.sh
@@ -1,10 +1,16 @@
+##########################
+USER='root'
+#HOST='h2970439.stratoserver.net'
+HOST='v2202310207729240622.luckysrv.de'
+##########################
+
 DOCKER_CONTAINER='backend'
 IMAGE_PATH='app/images'
 DATE='26Sep2023'
 
-ssh "root@h2970439.stratoserver.net" "docker cp /home/evento/backup/mongodb_backup/IMAGE_BACKUP/ ${DOCKER_CONTAINER}:/${IMAGE_PATH}"
-ssh "root@h2970439.stratoserver.net" "docker exec mongodb bash -c 'mkdir /backup'"
-ssh "root@h2970439.stratoserver.net" ""docker cp /home/evento/backup/mongodb_backup/${DATE}/ mongodb:/backup/${DATE}/""
+ssh "${USER}@${HOST}" "docker cp /home/evento/backup/mongodb_backup/IMAGE_BACKUP/ ${DOCKER_CONTAINER}:/${IMAGE_PATH}"
+ssh "${USER}@${HOST}" "docker exec mongodb bash -c 'mkdir /backup'"
+ssh "${USER}@${HOST}" ""docker cp /home/evento/backup/mongodb_backup/${DATE}/ mongodb:/backup/${DATE}/""
 
-ssh "root@h2970439.stratoserver.net" "docker exec mongodb bash -c 'mongorestore --gzip --uri mongodb://mongodb:27017/db_evento backup/${DATE}/daily_backup/db_evento'"
-ssh "root@h2970439.stratoserver.net" "docker exec mongodb bash -c 'rm -f -r backup/${DATE}'"
+ssh "${USER}@${HOST}" "docker exec mongodb bash -c 'mongorestore --gzip --uri mongodb://mongodb:27017/db_evento backup/${DATE}/daily_backup/db_evento'"
+ssh "${USER}@${HOST}" "docker exec mongodb bash -c 'rm -f -r backup/${DATE}'"
--- backend/backup_script/restore_from_local.sh
+++ backend/backup_script/restore_from_local.sh
@@ -1,4 +1,11 @@
-DOCKER_CONTAINER='backend'
+##########################
+USER='root'
+#HOST='h2970439.stratoserver.net'
+HOST='v2202310207729240622.luckysrv.de'
+##########################
+
+BACKEND_CONTAINER_LOCAL='evento_backend_container'
+BACKEND_CONTAINER_SEVRER='backend'
 IMAGE_PATH='app/images'
 
 DB_NAME=db_evento
@@ -7,18 +14,34 @@ BACKUP_PATH_MONGO="local_instant_dump"
 DB_BACKUP_PATH='/home/evento/backup/mongodb_instant_backup'
 IMAGE_PATH='app/images'
 
+#######
+# DB
+#######
+
+# LOCAL DEV DB DUMP
 # docker exec -i mongodb mongodump --host '0.0.0.0' --port '27017' --db ${DB_NAME} "" --out "backup/db_evento" --gzip
 # docker cp "mongodb:/backup/db_evento" "../backup_local_dev/db_backup/"
 
-ssh "root@h2970439.stratoserver.net" "mkdir ${DB_BACKUP_PATH}/${BACKUP_PATH_MONGO}/${DB_NAME}/"
-ssh "root@h2970439.stratoserver.net" "mkdir ${DB_BACKUP_PATH}/${BACKUP_PATH_MONGO}/IMAGE_BACKUP/"
-scp -r ../backup_local_dev/db_backup/. root@h2970439.stratoserver.net:${DB_BACKUP_PATH}/${BACKUP_PATH_MONGO}/${DB_NAME}/ 
-#scp -r ../backup_local_dev/image_backup/. root@h2970439.stratoserver.net:${DB_BACKUP_PATH}/${BACKUP_PATH_MONGO}/IMAGE_BACKUP/ 
+# TRANSER LOCAL DB TO SERVER 
+ssh "${USER}@${HOST}" "mkdir -p ${DB_BACKUP_PATH}/${BACKUP_PATH_MONGO}/${DB_NAME}/"
+scp -r ../backup_local_dev/db_backup/. ${USER}@${HOST}:${DB_BACKUP_PATH}/${BACKUP_PATH_MONGO}/${DB_NAME}/ 
 
-#ssh "root@h2970439.stratoserver.net" "docker cp ${DB_BACKUP_PATH}/${BACKUP_PATH_MONGO}/IMAGE_BACKUP/ ${DOCKER_CONTAINER}:/${IMAGE_PATH}"
+# COPY DB INTO MONGO DB DOCKER ON SERVER
+ssh "${USER}@${HOST}" "docker exec mongodb bash -c 'mkdir -p backup/${BACKUP_PATH_MONGO}/${DB_NAME}'"
+ssh "${USER}@${HOST}" ""docker cp ${DB_BACKUP_PATH}/${BACKUP_PATH_MONGO}/${DB_NAME}/. mongodb:/backup/${BACKUP_PATH_MONGO}/""
 
-ssh "root@h2970439.stratoserver.net" "docker exec mongodb bash -c 'mkdir -p backup/${BACKUP_PATH_MONGO}/${DB_NAME}'"
-ssh "root@h2970439.stratoserver.net" ""docker cp ${DB_BACKUP_PATH}/${BACKUP_PATH_MONGO}/${DB_NAME}/ mongodb:/backup/${BACKUP_PATH_MONGO}/""
+#########
+# IMAGES
+#########
 
-ssh "root@h2970439.stratoserver.net" "docker exec mongodb bash -c 'mongorestore --gzip --uri mongodb://mongodb:27017/db_evento backup/${BACKUP_PATH_MONGO}/${DB_NAME}'"
-ssh "root@h2970439.stratoserver.net" "docker exec mongodb bash -c 'rm -f -r backup/${BACKUP_PATH_MONGO}/${DB_NAME}'"
+#DUMP IMAGES FROM BACKEND
+mkdir "../backup_local_dev/IMAGE_BACKUP/"
+docker cp "${BACKEND_CONTAINER_LOCAL}:${IMAGE_PATH}/." "../backup_local_dev/IMAGE_BACKUP/"
+
+# TRANSER IMAGES TO SERVER
+ssh "${USER}@${HOST}" "mkdir -p ${DB_BACKUP_PATH}/${BACKUP_PATH_MONGO}/IMAGE_BACKUP/"
+scp -r ../backup_local_dev/image_backup/. ${USER}@${HOST}:${DB_BACKUP_PATH}/${BACKUP_PATH_MONGO}/IMAGE_BACKUP/ 
+
+# COPY IMAGES ON SERVER INTO DOCKER BACKEDND CONTAINER
+ssh "${USER}@${HOST}" "docker cp ${DB_BACKUP_PATH}/${BACKUP_PATH_MONGO}/IMAGE_BACKUP/. ${BACKEND_CONTAINER_SEVRER}:/${IMAGE_PATH}"
+
