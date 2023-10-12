9bbfa9ea40276f4961ae602986044b1d8ffa0215
mschaller <Maximilian.Schaller@jambit.com>
2023-10-12 21:49:42 +0200

    staging docker files

---
+++ evento_frontend/src/environments/environment.staging.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'https://staging.backend.evento-leipzig.de'
+};
---
+++ evento_insert/src/environments/environment.staging.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiBaseUrl: 'https://staging.backend.evento-leipzig.de'
+};
