e32397fc6a5ce8489053841e1edadf34222fb8bf
mschaller <Maximilian.Schaller@jambit.com>
2023-10-13 09:55:09 +0200

    allow Cors

--- backend/app.js
+++ backend/app.js
@@ -18,12 +18,13 @@ const allowedOrigins = [
   "http://localhost:4200",
   "http://localhost:4201",
   "https://insert.evento-leipzig.de",
-  "https://staging.insert.evento-leipzig.de",
+  "https://insert.staging.evento-leipzig.de",
   "https://evento-leipzig.de",
-  "https:/staging.evento-leipzig.de",
+  "https://staging.evento-leipzig.de",
   "https://www.evento-leipzig.de",
+  "https://www.staging.evento-leipzig.de",
   "https://backend.evento-leipzig.de",
-  "https://staging.backend.evento-leipzig.de",
+  "https://backend.staging.evento-leipzig.de",
 
 ];
 
