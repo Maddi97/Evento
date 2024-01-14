0f04c04eb2ed33821f903460cb2b8dcb97b1af9e
Maddi97 <sgdmaddi.97@gmail.com>
2024-01-14 09:43:51 +0700

    fix: search filter dont load on empty reset

--- evento_frontend/src/app/common-utilities/search-filter/search-filter.component.ts
+++ evento_frontend/src/app/common-utilities/search-filter/search-filter.component.ts
@@ -1,18 +1,18 @@
-import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { Capacitor } from '@capacitor/core';
-import { Keyboard } from '@capacitor/keyboard';
-import { Subscription, filter } from 'rxjs';
-import { clearSearchFilter } from '../logic/search-filter-helper';
-import { SessionStorageService } from '../session-storage/session-storage.service';
+import { Component, HostListener, OnDestroy, OnInit } from "@angular/core";
+import { NavigationEnd, Router } from "@angular/router";
+import { Capacitor } from "@capacitor/core";
+import { Keyboard } from "@capacitor/keyboard";
+import { Subscription, filter } from "rxjs";
+import { clearSearchFilter } from "../logic/search-filter-helper";
+import { SessionStorageService } from "../session-storage/session-storage.service";
 
 @Component({
-  selector: 'app-search-filter',
-  templateUrl: './search-filter.component.html',
-  styleUrls: ['./search-filter.component.css']
+  selector: "app-search-filter",
+  templateUrl: "./search-filter.component.html",
+  styleUrls: ["./search-filter.component.css"],
 })
 export class SearchFilterComponent implements OnInit, OnDestroy {
-  searchString = ''
+  searchString = "";
   timeout = null;
   delay = 300;
   getScreenWidth;
@@ -20,8 +20,8 @@ export class SearchFilterComponent implements OnInit, OnDestroy {
   event$: Subscription;
   constructor(
     private sessionStorageService: SessionStorageService,
-    private router: Router,
-  ) { }
+    private router: Router
+  ) {}
   ngOnDestroy(): void {
     clearTimeout(this.timeout);
     this.event$.unsubscribe();
@@ -29,19 +29,19 @@ export class SearchFilterComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.getScreenWidth = window.innerWidth;
-    this.event$ = this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe((event: NavigationEnd) => {
-      clearTimeout(this.timeout);
-      clearSearchFilter(this.sessionStorageService)
-    });
+    this.event$ = this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        clearTimeout(this.timeout);
+        clearSearchFilter(this.sessionStorageService);
+      });
   }
 
   onSearchChange() {
     clearTimeout(this.timeout);
     this.timeout = setTimeout(() => {
       this.sessionStorageService.setSearchString(this.searchString);
-    }, this.delay)
+    }, this.delay);
   }
   @HostListener("window:resize")
   getScreenSize() {
@@ -50,42 +50,40 @@ export class SearchFilterComponent implements OnInit, OnDestroy {
   @HostListener("document:scroll")
   hideSearchOnScroll() {
     const inputElement = document.getElementById("searchright");
-    inputElement.classList.remove('focus');
-    inputElement.blur()
-    this.isFocused = false
+    inputElement.classList.remove("focus");
+    inputElement.blur();
+    this.isFocused = false;
   }
 
   @HostListener("document:touch", ["$event"])
   @HostListener("document:mousedown", ["$event"])
   public onMouseDownTrigger(event: any) {
-    const inputElement: HTMLInputElement = event.srcElement
+    const inputElement: HTMLInputElement = event.srcElement;
     if (inputElement.id !== "searchright") {
-      const inputBar = document.getElementById("searchright")
+      const inputBar = document.getElementById("searchright");
       if (inputElement.id === "searchlabel") {
         if (!this.isFocused) {
-          inputBar.classList.add("focus")
-
-        }
-        else {
-          clearSearchFilter(this.sessionStorageService)
-          inputBar.classList.remove("focus")
+          inputBar.classList.add("focus");
+        } else {
+          if (this.searchString.length > 0) {
+            clearSearchFilter(this.sessionStorageService);
+          }
+          inputBar.classList.remove("focus");
           setTimeout(() => {
             if (Capacitor.isNativePlatform()) {
-              Keyboard.hide()
+              Keyboard.hide();
             }
-          }, 10)
+          }, 10);
         }
-      }
-      else {
+      } else {
         if (this.isFocused && !inputElement.value) {
-          inputBar.classList.remove("focus")
-          inputBar.blur()
+          inputBar.classList.remove("focus");
+          inputBar.blur();
           setTimeout(() => {
             if (Capacitor.isNativePlatform()) {
-              Keyboard.hide()
+              Keyboard.hide();
             }
-          }, 10)
-
+          }, 10);
         }
       }
       if (this.isFocused || event.srcElement.id === "searchlabel") {
