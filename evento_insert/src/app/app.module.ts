16b983d85b38ac0d0d9b0013b3cf92f89e84791d
Maddi97 <sgdmaddi.97@gmail.com>
2024-02-21 15:33:08 +0700

    refactor: settings to molecules

--- evento_insert/src/app/app.module.ts
+++ evento_insert/src/app/app.module.ts
@@ -1,61 +1,22 @@
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { EventViewComponent } from "./pages/event-view/event-view.component";
 
-import { MatAutocompleteModule } from "@angular/material/autocomplete";
-import { MatButtonModule } from "@angular/material/button";
-import { MatCardModule } from "@angular/material/card";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatChipsModule } from "@angular/material/chips";
-import { MatDatepickerModule } from "@angular/material/datepicker";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatDividerModule } from "@angular/material/divider";
-import { MatExpansionModule } from "@angular/material/expansion";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { MatRadioModule } from "@angular/material/radio";
-import { MatSelectModule } from "@angular/material/select";
-import { MatSlideToggleModule } from "@angular/material/slide-toggle";
-import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { NgxSpinnerModule } from "ngx-spinner";
 import { SelectionViewComponent } from "./shared/molecules/selection-view/selection-view.component";
 
 import { HttpClientModule } from "@angular/common/http";
 import { authInterceptorProviders } from "@shared/services/auth/auth.interceptor";
 
-import { OrganizerViewComponent } from "./pages/organizer-view/organizer-view.component";
-
 import {
   CommonModule,
   LocationStrategy,
   PathLocationStrategy,
 } from "@angular/common";
-import { ReactiveFormsModule } from "@angular/forms";
-import { MatNativeDateModule } from "@angular/material/core";
-import { CustomDialogComponent } from "@atoms/custom-dialog/custom-dialog.component";
-import { LoginComponent } from "./pages/login/login.component";
-import { CategorySelectComponent } from "./shared/molecules/category-select/category-select.component";
-import { CategoryViewComponent } from "./pages/category-view/category-view.component";
-import { EventCrawledComponent } from "./shared/atoms/atom-event-crawled/event-crawled.component";
-import { CrawlEventsComponent } from "./pages/crawl-events/crawl-events.component";
-import { CrawledEventsToEventComponent } from "./shared/molecules/crawled-events-to-event/crawled-events-to-event.component";
-import { EventFormComponent } from "./shared/molecules/event-form/event-form.component";
-import { MapViewComponent } from "./shared/molecules/map-view/map-view.component";
-import { OrganizerFormComponent } from "./shared/molecules/organizer-form/organizer-form.component";
 import { AuthGuardService } from "@shared/services/auth/auth.guard.service";
-import { AutocompleteOrganizerComponent } from "@atoms/autocomplete-organizer/autocomplete-organizer.component";
-import { SelectionListComponent } from "@atoms/selection-list/selection-list.component";
-import { EventFrequencyFormComponent } from "@atoms/event-frequency-form/event-frequency-form.component";
-import { GlobalSettingsComponent } from "./pages/settings/global-settings/global-settings.component";
-import { GlobalSettingsFormComponent } from "./pages/settings/global-settings-form/global-settings-form.component";
-import { ExpansionPanelComponent } from "./shared/molecules/expansion-panel/expansion-panel.component";
-import { EventPanelComponent } from "@atoms/event-panel/event-panel.component";
-import { CategoryEventExpansionPanelComponent } from "./shared/molecules/category-event-expansion-panel/category-event-expansion-panel.component";
 
 @NgModule({
   declarations: [AppComponent],
--- evento_insert/src/app/pages/settings/global-settings/global-settings.component.ts
+++ evento_insert/src/app/pages/settings/global-settings/global-settings.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { SettingsService } from "@shared/services/settings/settings.service.service";
 import { Settings } from "@globals/models/settings";
 import { CommonModule } from "@angular/common";
-import { GlobalSettingsFormComponent } from "../global-settings-form/global-settings-form.component";
+import { GlobalSettingsFormComponent } from "../../../shared/molecules/global-settings-form/global-settings-form.component";
 @Component({
   selector: "app-global-settings",
   standalone: true,
