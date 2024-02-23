72a9a2a74a39a88b8102bbf6308beb0fb3c585da
Maddi97 <sgdmaddi.97@gmail.com>
2024-02-23 13:20:45 +0700

    tests: some fixes

--- evento_frontend/src/app/shared/pipes/BypassSecurity.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ByPassSecurityPipe } from "./BypassSecurity.pipe";
-
-describe("SanitizePipe", () => {
-  it("create an instance", () => {
-    const pipe = new ByPassSecurityPipe(null);
-    expect(pipe).toBeTruthy();
-  });
-});
