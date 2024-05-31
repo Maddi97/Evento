021fdfe31007225ccceb238d808e90202f83db14
Maddi97 <sgdmaddi.97@gmail.com>
2024-05-31 14:04:13 +0200

    ATTENTION: CHANGE ACCESS RIGHTS ON FILES

--- ai_crawler_server/scripts/data_backup_script.sh
+++ ai_crawler_server/scripts/data_backup_script.sh
@@ -5,8 +5,8 @@ export PATH=/bin:/usr/bin:/usr/local/bin
 DOCKER_CONTAINER='crawler_container'
 DATASET_PATH='app/datasets'
 TODAY=`date +"%d%b%Y"`
-BACKUP_PATH = '/home/evento'
+BACKUP_PATH='/home/evento/datasets'
 mkdir -p ${BACKUP_PATH}/${TODAY}
 
 docker cp ${DOCKER_CONTAINER}:/${DATASET_PATH} ${BACKUP_PATH}/${TODAY}
-docker exec -i ${DOCKER_CONTAINER} bash -c rm -f -r /${DATASET_PATH}/
+docker exec -i ${DOCKER_CONTAINER} rm -r -f /${DATASET_PATH}/
--- evento_insert/src/app/pages/settings/global-settings/global-settings.component.ts
+++ evento_insert/src/app/pages/settings/global-settings/global-settings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { SettingsService } from "@shared/services/settings/settings.service.service";
+import { StoreDatasetService } from "@shared/services/store-dataset/store-dataset.service";
 import { Settings } from "@globals/models/settings";
 import { CommonModule } from "@angular/common";
 import { GlobalSettingsFormComponent } from "../../../shared/molecules/global-settings-form/global-settings-form.component";
@@ -12,7 +13,10 @@ import { GlobalSettingsFormComponent } from "../../../shared/molecules/global-se
 })
 export class GlobalSettingsComponent implements OnInit {
   settings: Settings;
-  constructor(private settingsService: SettingsService) {}
+  constructor(
+    private settingsService: SettingsService,
+    private storeDatasetService: StoreDatasetService
+  ) {}
   ngOnInit(): void {
     this.settingsService.getSettings().subscribe((settings) => {
       this.settings = settings;
@@ -29,5 +33,21 @@ export class GlobalSettingsComponent implements OnInit {
         this.settings = settings;
       });
     }
+  }
+  testStore() {
+    this.storeDatasetService
+      .storeEventDetailsDataset([
+        {
+          name: "pimmel",
+          pimmel: "name",
+          link: "https://www.urbanite.net/leipzig/events/linc-2024/2024-05-30/",
+        },
+        {
+          name: "pimmel2",
+          pimmel: "name2",
+          link: "https://www.urbanite.net/leipzig/events/linc-2024/2024-05-30/",
+        },
+      ])
+      .subscribe();
   }
 }
