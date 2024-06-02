c72a8b2b48cc942754b7f22625a1610ed876fd62
Maddi97 <sgdmaddi.97@gmail.com>
2024-06-02 15:45:10 +0200

    fix css

--- evento_frontend/src/app/shared/molecules/map-view/map-view.component.ts
+++ evento_frontend/src/app/shared/molecules/map-view/map-view.component.ts
@@ -39,6 +39,8 @@ export class MapViewComponent implements OnInit, OnChanges {
   @Input() centerMapOnPosition: Array<Number>;
   @Input() hasMoreEvents: boolean;
   @Input() showInputBar: boolean = true;
+  @Input() isFullEventPage: boolean = false;
+
   @Output() emitClickedEventId: EventEmitter<any> = new EventEmitter<any>();
 
   isPositionDefault: boolean;
